5804623867abe9a0e3ddf6d3b0fd27e8f75d1fce
Bricktech <bricktech2000@gmail.com>
2022-03-27 06:50:23 -0400

    removed default 500 luna wallet

--- contracts/will/src/error.rs
+++ contracts/will/src/error.rs
@@ -20,6 +20,9 @@ pub enum ContractError {
 
     #[error("InvalidRecipientPercentage ")]
     InvalidRecipientPercentage {},
+
+    #[error("NotEnoughTime  ")]
+    NotEnoughTime {},
     // Add any other custom errors you like here.
     // Look at https://docs.rs/thiserror/1.0.21/thiserror/ for details.
 }
